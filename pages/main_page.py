d194d67f6e7937134508693e9c66487cdaa30237
Dmitry Volkow <Dimya92@gmail.com>
2022-06-16 18:21:57 +0300

    rename function

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     
-    def test_guest_can_go_to_login_page(self):
+    def go_to_login_page(self):
         
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
